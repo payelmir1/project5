c935792bc786ff4e7700de73ace1f8aa8b952125
programmingShikbo <usamanifestation2@gmail.com>
2024-11-02 10:18:34 +0600

    First commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const addTask = () => {
+    const taskInput = document.getElementById('taskInput');
+    const taskInputValue = taskInput.value.trim();
+    
+    
+    
+    if(taskInputValue !== ""){
+        const taskListUl = document.getElementById("taskList");
+        const li = document.createElement('li');
+        li.innerHTML = '<input type="checkbox"> ' + taskInputValue + ' <button onclick = "deleteTask(this)">Delete</button>';
+        taskListUl.appendChild(li);
+
+        taskInput.value = "";
+    }
+    else{
+        alert("Please Add a Task");
+    }
+    
+    
+}
+
+const deleteTask = (btn) => {
+ 
+   const li = btn.parentNode;
+   li.parentNode.removeChild(li);
+   
+   
+}
+
+const deletedTaskChecked = () => {
+    const taskList = document.getElementById('taskList');
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    
+    checkboxes.forEach(checkbox => {
+
+       const li = checkbox.parentNode;
+       li.parentNode.removeChild(li);
+        
+    })
+    
+    
+}
